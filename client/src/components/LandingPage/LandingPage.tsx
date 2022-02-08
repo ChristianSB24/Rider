f4e10bb961c1b6f15ce9041ca3d7c439386e7484
christian saldana <christiansaldana10@gmail.com>
2022-02-07 22:42:47 -0600

    Completed dashboard

--- client/src/components/LandingPage/LandingPage.tsx
+++ client/src/components/LandingPage/LandingPage.tsx
@@ -34,20 +34,39 @@ export const LandingPage = () => {
                         <img src={RotateChecklist} className="card-img dashboard-main-img" />
                         <div className="card-img-overlay d-flex flex-column left-0">
                             <p className="card-title text-start text-white t2">Errands just got<br />easier</p>
-                            <p className='t3'>Ride with Rider <i className="bi bi-arrow-right"></i></p>
+                            <p className='t5'>Ride with Rider <i className="bi bi-arrow-right"></i></p>
                         </div>
                     </div>
                     <div className="d-flex flex-row justify-content-between w-100 mb-3">
-                        <button className='btn btn-grey-secondary dashboard-buttons pt-0 pb-1'><img className="dashboard-main-img mb-1" src={RiderCarIcon} /><p className='m-0 t2'>Ride</p></button>
-                        <button className='btn btn-grey-secondary dashboard-buttons p-0 pb-1'><img className="dashboard-main-img" src={RiderFoodIcon} /><p className='m-0 t2'>Food</p></button>
-                        <button className='btn btn-grey-secondary dashboard-buttons p-0 pb-1'><img className="dashboard-main-img" src={RiderPackageIcon} /><p className='m-0 t2'>Package</p></button>
+                        <button className='btn btn-grey-secondary dashboard-buttons pt-0 pb-1'><img className="dashboard-main-img" src={RiderCarIcon} /><p className='m-0 t6'>Ride</p></button>
+                        <button className='btn btn-grey-secondary dashboard-buttons p-0 pb-1'><img className="dashboard-main-img" src={RiderFoodIcon} /><p className='m-0 t6'>Food</p></button>
+                        <button className='btn btn-grey-secondary dashboard-buttons p-0 pb-1'><img className="dashboard-main-img" src={RiderPackageIcon} /><p className='m-0 t6'>Package</p></button>
                     </div>
-                    <div className="d-flex justify-content-between flex-row bg-grey-secondary w-100 p-1 ps-3 pe-2 mb-3">
-                        <div className="t1 fw-normal align-self-center">Where to?</div>
+                    <div className="d-flex justify-content-between flex-row bg-grey-secondary w-100 p-2 ps-3 pe-2 mb-4">
+                        <div className="t1 fw-600 align-self-center">Where to?</div>
                         <div className="border-start border-grey p-1 ps-4 text-primary align-self-center">
-                            <div className="bg-white rounded-pill p-1 px-2 t3"><i className="bi bi-clock-fill pe-2"></i>Now<i className="bi bi-caret-down-fill ps-2"></i></div>
+                            <div className="bg-white rounded-pill p-1 px-2 t4"><i className="bi bi-clock-fill pe-2"></i>Now<i className="bi bi-caret-down-fill ps-2"></i></div>
                         </div>
                     </div>
+                    <div className="d-flex justify-content-between align-items-center w-100 ps-2 mb-4">
+                        <div className="d-flex justify-content-center align-items-center rounded-circle bg-grey-secondary p-2 mb-4 star-wrapper">
+                            <i className="bi bi-star-fill rounded-circle t4"></i>
+                        </div>
+                        <div className="d-flex ms-2 justify-content-between border-bottom pb-4 w-100">
+                            <div className="fw-600 t3">Choose a saved place</div>
+                            <i className="bi bi-caret-right-fill t4"></i>
+                        </div>
+                    </div>
+                    <div className="d-flex justify-content-between align-items-center w-100 ps-2 mb-2">
+                        <div className="d-flex justify-content-center align-items-center rounded-circle bg-grey-secondary p-2 mb-4 star-wrapper">
+                        <i className="bi bi-geo-fill t3"></i>
+                        </div>
+                        <div className="d-flex ms-2 justify-content-between border-bottom pb-4 w-100">
+                            <div className="fw-600 t3">Set destination on map</div>
+                            <i className="bi bi-caret-right-fill t3"></i>
+                        </div>
+                    </div>
+                    <div className="align-self-start t3 mb-4 ps-1 fw-600 ">Around you</div>
                     <Map lat={lat} lng={lng} zoom={zoom} pickUpAddress={''} dropOffAddress={''}/>
                     <h1>Taxi</h1>
                     <DashboardButton />
