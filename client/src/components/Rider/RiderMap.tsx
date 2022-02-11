bb5172f2011ab69eb4aefb15a972d2aa9f4401e8
christian saldana <christiansaldana10@gmail.com>
2022-02-10 18:46:05 -0600

    Working on map input

--- client/src/components/Rider/RiderMap.tsx
+++ client/src/components/Rider/RiderMap.tsx
@@ -20,6 +20,18 @@ const RiderMap = () => {
     return (
     <>
         <Link to='/' className='position-absolute map-arrow m-2'><i className="bi bi-arrow-left-circle-fill"></i></Link>
+        <div className='d-flex flex-column position-absolute map-input bg-white p-3 w-100'>
+        <div className="d-flex flex-column justify-content-start">
+            <i className="bi bi-arrow-left t0"></i>
+        </div>
+        <div className='d-flex flex-row'>
+            <i className="bi bi-file-arrow-down align-self-center t0"></i>
+            <div className='d-flex justify-content-center flex-column flex-grow-1 pe-5'>
+                <input />
+                <input />
+            </div>
+        </div>
+        </div>
         <Map
             lat={lat} 
             lng={lng} 
@@ -28,7 +40,7 @@ const RiderMap = () => {
             dropOffAddress={''} 
             className={{ width: '100%', height: `${height}px`}} />
         
-        <div className='position-absolute map-input w-100 pb-5 pt-3 px-3 bg-white'>
+        <div className='position-absolute map-button w-100 pb-5 pt-3 px-3 bg-white'>
             <Link to='/' className='text-decoration-none'><div className="w-100 p-2 px-3 bg-grey-secondary t1 fw-600">Search destination</div></Link>
         </div>
     </>
